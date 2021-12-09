6acc93d1dfe91abd803ecf6f7de12d0381b73667
Sayu <86679476+xcz404@users.noreply.github.com>
2021-12-09 20:02:37 +0700

    Update index.js

--- config/index.js
+++ config/index.js
@@ -3,18 +3,18 @@ const fs = require("fs");
 
 global.restAPIs = "https://api-xcoders.xyz";
 global.apikeys = "your apikey";
-global.packname = "XCodersBOT";
-global.author = "By Farhannn";
+global.packname = "Sayu Bot";
+global.author = "By Rafaa";
 global.watermark = "𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲 𝐅𝐚𝐫𝐡𝐚𝐧𝐧𝐧𝐧𝐧";
-global.ownerNumber = ["62xxxxxxxxxx@s.whatsapp.net"];
+global.ownerNumber = ["6285861870154@s.whatsapp.net"];
 global.thumbnails = fs.readFileSync("./image/thumbnail.png");
 global.banChats = false;
 global.offline = false;
-global.multiprefix = false;
+global.multiprefix = true;
 global.nonprefix = false;
 global.prefix = "#";
 global.browserDescription = {
-  set_name: "Xcoders",
+  set_name: "SAYUOFC",
   set_browser: "Safari"
 };
 global.response = {
