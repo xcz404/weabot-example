e8054f30a408b358856f98ff021fbc044dc57726
Sayu <86679476+xcz404@users.noreply.github.com>
2021-12-09 21:25:54 +0700

    Update index.js

--- helpers/index.js
+++ helpers/index.js
@@ -3,191 +3,139 @@ const fs = require("fs");
 
 const m = '```';
 const b = '*';
-const Features = (prefix) => {
+const Features = (prefix, pushname2, Ucapan) => {
   let number = 0;
-  return `\t\t꒷꒦꒷꒦꒷꒷꒦꒷꒦꒷꒷꒦꒷꒦꒷꒦꒷꒷꒦꒷꒦꒷꒷꒦꒷꒦꒷꒦꒷\n\n\n
-\t\t\t\t\t</ ${b}DOWNLOADER${b} >
+  return `Hai ${pushname}, ${Ucapan}
 
-${m}[ ${number += 1} ] ${prefix}ytshort${m}
-${m}[ ${number += 1} ] ${prefix}ytmp3${m}
-${m}[ ${number += 1} ] ${prefix}ytmp4${m}
-${m}[ ${number += 1} ] ${prefix}fbdl${m}
-${m}[ ${number += 1} ] ${prefix}joox${m}
-${m}[ ${number += 1} ] ${prefix}igdl${m}
-${m}[ ${number += 1} ] ${prefix}igtv${m}
-${m}[ ${number += 1} ] ${prefix}tiktok${m}
-${m}[ ${number += 1} ] ${prefix}soundcloud${m}
-${m}[ ${number += 1} ] ${prefix}ifunny${m}
-${m}[ ${number += 1} ] ${prefix}imdb${m}
-${m}[ ${number += 1} ] ${prefix}twitter${m}
-${m}[ ${number += 1} ] ${prefix}cocofun${m}
-${m}[ ${number += 1} ] ${prefix}stickerline${m}
-${m}[ ${number += 1} ] ${prefix}pinterest${m}
-${m}[ ${number += 1} ] ${prefix}xvideosdl${m}
-${m}[ ${number += 1} ] ${prefix}xnxxdl${m}
 
-\t\t\t\t\t</ ${b}ISLAMIC${b} >
 
-${m}[ ${number += 1} ] ${prefix}jadwalsholat${m}
-${m}[ ${number += 1} ] ${prefix}asmaulhusna${m}
-${m}[ ${number += 1} ] ${prefix}kisahnabi${m}
-${m}[ ${number += 1} ] ${prefix}hadist${m}
-${m}[ ${number += 1} ] ${prefix}quran${m}
-${m}[ ${number += 1} ] ${prefix}surahnumber${m}
 
-\t\t\t\t\t</ ${b}STALKER${b} >
+${b}Information${b}
+${number += 1} > ${prefix}trendtweet${m}
+${number += 1} > ${prefix}trendyt${m}
+${number += 1} > ${prefix}trendytgaming${m}
+${number += 1} > ${prefix}infogempa${m}
+${number += 1} > ${prefix}rsrujukan${m}
+${number += 1} > ${prefix}covidindo${m}
+${number += 1} > ${prefix}covidworld${m}
+${number += 1} > ${prefix}kbbi${m}
+${number += 1} > ${prefix}infocuaca${m}
 
-${m}[ ${number += 1} ] ${prefix}ttstalk${m}
-${m}[ ${number += 1} ] ${prefix}igstalk${m}
-${m}[ ${number += 1} ] ${prefix}githubstalk${m}
+${b}Downloader${b}
+${number += 1} > ${prefix}ytshort${m}
+${number += 1} > ${prefix}ytmp3${m}
+${number += 1} > ${prefix}ytmp4${m}
+${number += 1} > ${prefix}fbdl${m}
+${number += 1} > ${prefix}joox${m}
+${number += 1} > ${prefix}igdl${m}
+${number += 1} > ${prefix}igtv${m}
+${number += 1} > ${prefix}tiktok${m}
+${number += 1} > ${prefix}soundcloud${m}
+${number += 1} > ${prefix}ifunny${m}
+${number += 1} > ${prefix}imdb${m}
+${number += 1} > ${prefix}twitter${m}
+${number += 1} > ${prefix}cocofun${m}
+${number += 1} > ${prefix}stickerline${m}
+${number += 1} > ${prefix}pinterest${m}
+${number += 1} > ${prefix}xvideosdl${m}
+${number += 1} > ${prefix}xnxxdl${m}
 
-\t\t\t\t\t</ ${b}MAKER${b} >
+${b}Islami${b}
+${number += 1} > ${prefix}jadwalsholat${m}
+${number += 1} > ${prefix}asmaulhusna${m}
+${number += 1} > ${prefix}kisahnabi${m}
+${number += 1} > ${prefix}hadist${m}
+${number += 1} > ${prefix}quran${m}
+${number += 1} > ${prefix}surahnumber${m}
 
-${m}[ ${number += 1} ] ${prefix}sticker${m}
-${m}[ ${number += 1} ] ${prefix}nulis${m}
-${m}[ ${number += 1} ] ${prefix}ssweb${m}
-${m}[ ${number += 1} ] ${prefix}wasted${m}
-${m}[ ${number += 1} ] ${prefix}wanted${m}
-${m}[ ${number += 1} ] ${prefix}jail${m}
-${m}[ ${number += 1} ] ${prefix}circle${m}
-${m}[ ${number += 1} ] ${prefix}delete${m}
-${m}[ ${number += 1} ] ${prefix}invert${m}
-${m}[ ${number += 1} ] ${prefix}sepia${m}
-${m}[ ${number += 1} ] ${prefix}joke${m}
-${m}[ ${number += 1} ] ${prefix}blur${m}
-${m}[ ${number += 1} ] ${prefix}gay${m}
-${m}[ ${number += 1} ] ${prefix}facepalm${m}
-${m}[ ${number += 1} ] ${prefix}beautiful${m}
-${m}[ ${number += 1} ] ${prefix}trash${m}
-${m}[ ${number += 1} ] ${prefix}instagram${m}
-${m}[ ${number += 1} ] ${prefix}scary${m}
-${m}[ ${number += 1} ] ${prefix}glitch${m}
-${m}[ ${number += 1} ] ${prefix}ps4${m}
-${m}[ ${number += 1} ] ${prefix}rejected${m}
-${m}[ ${number += 1} ] ${prefix}brazzers${m}
-${m}[ ${number += 1} ] ${prefix}tahta${m}
-${m}[ ${number += 1} ] ${prefix}costumtahta${m}
-${m}[ ${number += 1} ] ${prefix}badut${m}
-${m}[ ${number += 1} ] ${prefix}badut2${m}
-${m}[ ${number += 1} ] ${prefix}badut3${m}
+${b}Stalker${b}
+${number += 1} > ${prefix}ttstalk${m}
+${number += 1} > ${prefix}igstalk${m}
+${number += 1} > ${prefix}githubstalk${m}
 
-\t\t\t\t\t</ ${b}PHOTOOXY${b} >
+${b}Maker / Convert${b}
+${number += 1} > ${prefix}sticker${m}
+${number += 1} > ${prefix}nulis${m}
+${number += 1} > ${prefix}ssweb${m}
+${number += 1} > ${prefix}wasted${m}
+${number += 1} > ${prefix}wanted${m}
+${number += 1} > ${prefix}jail${m}
+${number += 1} > ${prefix}circle${m}
+${number += 1} > ${prefix}delete${m}
+${number += 1} > ${prefix}invert${m}
+${number += 1} > ${prefix}sepia${m}
+${number += 1} > ${prefix}joke${m}
+${number += 1} > ${prefix}blur${m}
+${number += 1} > ${prefix}gay${m}
+${number += 1} > ${prefix}facepalm${m}
+${number += 1} > ${prefix}beautiful${m}
+${number += 1} > ${prefix}trash${m}
+${number += 1} > ${prefix}instagram${m}
+${number += 1} > ${prefix}scary${m}
+${number += 1} > ${prefix}glitch${m}
+${number += 1} > ${prefix}ps4${m}
+${number += 1} > ${prefix}rejected${m}
+${number += 1} > ${prefix}brazzers${m}
+${number += 1} > ${prefix}tahta${m}
+${number += 1} > ${prefix}costumtahta${m}
+${number += 1} > ${prefix}badut${m}
+${number += 1} > ${prefix}badut2${m}
+${number += 1} > ${prefix}badut3${m}
 
-${m}[ ${number += 1} ] ${prefix}oceansea${m}
-${m}[ ${number += 1} ] ${prefix}shadow${m}
-${m}[ ${number += 1} ] ${prefix}rainbow${m}
-${m}[ ${number += 1} ] ${prefix}gravity${m}
-${m}[ ${number += 1} ] ${prefix}burnpaper${m}
-${m}[ ${number += 1} ] ${prefix}smoke${m}
-${m}[ ${number += 1} ] ${prefix}romantic${m}
-${m}[ ${number += 1} ] ${prefix}naruto${m}
-${m}[ ${number += 1} ] ${prefix}lovemessage${m}
-${m}[ ${number += 1} ] ${prefix}hadist${m}
-${m}[ ${number += 1} ] ${prefix}grass${m}
-${m}[ ${number += 1} ] ${prefix}doubleheart${m}
-${m}[ ${number += 1} ] ${prefix}coffecup${m}
-${m}[ ${number += 1} ] ${prefix}lovetext${m}
-${m}[ ${number += 1} ] ${prefix}butterfly${m}
+${b}Wibu Kya<3${b}
+${number += 1} > ${prefix}kusonime${m}
+${number += 1} > ${prefix}manga${m}
+${number += 1} > ${prefix}loli${m}
+${number += 1} > ${prefix}waifu${m}
+${number += 1} > ${prefix}neko${m}
+${number += 1} > ${prefix}husbu${m}
 
-\t\t\t\t\t</ ${b}TEXTPROME${b} >
+${b}Random${b}
+${number += 1} > ${prefix}ppcouple${m}
+${number += 1} > ${prefix}cerpen${m}
+${number += 1} > ${prefix}meme${m}
+${number += 1} > ${prefix}darkjoke${m}
+${number += 1} > ${prefix}randomgore${m}
+${number += 1} > ${prefix}cersex${m}
+${number += 1} > ${prefix}randombokep${m}
 
-${m}[ ${number += 1} ] ${prefix}layered${m}
-${m}[ ${number += 1} ] ${prefix}pornhub${m}
-${m}[ ${number += 1} ] ${prefix}aglitch${m}
-${m}[ ${number += 1} ] ${prefix}embossed${m}
-${m}[ ${number += 1} ] ${prefix}paper${m}
-${m}[ ${number += 1} ] ${prefix}harrypotter${m}
-${m}[ ${number += 1} ] ${prefix}broken${m}
-${m}[ ${number += 1} ] ${prefix}blackpink${m}
-${m}[ ${number += 1} ] ${prefix}carbon${m}
-${m}[ ${number += 1} ] ${prefix}gradient${m}
-${m}[ ${number += 1} ] ${prefix}glue${m}
-${m}[ ${number += 1} ] ${prefix}blood${m}
-${m}[ ${number += 1} ] ${prefix}firework${m}
-${m}[ ${number += 1} ] ${prefix}dropwater${m}
-${m}[ ${number += 1} ] ${prefix}imglitch${m}
-${m}[ ${number += 1} ] ${prefix}glossy${m}
-${m}[ ${number += 1} ] ${prefix}bear${m}
-${m}[ ${number += 1} ] ${prefix}devil${m}
-${m}[ ${number += 1} ] ${prefix}christmas${m}
-${m}[ ${number += 1} ] ${prefix}magma${m}
-${m}[ ${number += 1} ] ${prefix}stone${m}
-${m}[ ${number += 1} ] ${prefix}light${m}
-${m}[ ${number += 1} ] ${prefix}berry${m}
-${m}[ ${number += 1} ] ${prefix}transformer${m}
+${b}Searching${b}
+${number += 1} > ${prefix}ttsearch${m}
+${number += 1} > ${prefix}ytsearch${m}
+${number += 1} > ${prefix}bokepsearch${m}
 
-\t\t\t\t\t</ ${b}INFORMATION${b} >
+${b}Tools${b}
+${number += 1} > ${prefix}tourl${m}
+${number += 1} > ${prefix}tinyurl${m}
 
-${m}[ ${number += 1} ] ${prefix}trendtweet${m}
-${m}[ ${number += 1} ] ${prefix}trendyt${m}
-${m}[ ${number += 1} ] ${prefix}trendytgaming${m}
-${m}[ ${number += 1} ] ${prefix}infogempa${m}
-${m}[ ${number += 1} ] ${prefix}rsrujukan${m}
-${m}[ ${number += 1} ] ${prefix}covidindo${m}
-${m}[ ${number += 1} ] ${prefix}covidworld${m}
-${m}[ ${number += 1} ] ${prefix}kbbi${m}
-${m}[ ${number += 1} ] ${prefix}infocuaca${m}
+${b}Groups${b}
+${number += 1} > ${prefix}group${m}
+${number += 1} > ${prefix}add${m}
+${number += 1} > ${prefix}kick${m}
+${number += 1} > ${prefix}promote${m}
+${number += 1} > ${prefix}demote${m}
+${number += 1} > ${prefix}adminlist${m}
+${number += 1} > ${prefix}del${m}
 
-\t\t\t\t\t</ ${b}ANIME${b} >
-
-${m}[ ${number += 1} ] ${prefix}kusonime${m}
-${m}[ ${number += 1} ] ${prefix}manga${m}
-${m}[ ${number += 1} ] ${prefix}loli${m}
-${m}[ ${number += 1} ] ${prefix}waifu${m}
-${m}[ ${number += 1} ] ${prefix}neko${m}
-${m}[ ${number += 1} ] ${prefix}husbu${m}
-
-\t\t\t\t\t</ ${b}RANDOM${b} >
-
-${m}[ ${number += 1} ] ${prefix}ppcouple${m}
-${m}[ ${number += 1} ] ${prefix}cerpen${m}
-${m}[ ${number += 1} ] ${prefix}meme${m}
-${m}[ ${number += 1} ] ${prefix}darkjoke${m}
-${m}[ ${number += 1} ] ${prefix}randomgore${m}
-${m}[ ${number += 1} ] ${prefix}cersex${m}
-${m}[ ${number += 1} ] ${prefix}randombokep${m}
-
-\t\t\t\t\t</ ${b}SEARCHING${b} >
-
-${m}[ ${number += 1} ] ${prefix}ttsearch${m}
-${m}[ ${number += 1} ] ${prefix}ytsearch${m}
-${m}[ ${number += 1} ] ${prefix}bokepsearch${m}
-
-\t\t\t\t\t</ ${b}TOOLS${b} >
-
-${m}[ ${number += 1} ] ${prefix}tourl${m}
-${m}[ ${number += 1} ] ${prefix}tinyurl${m}
-
-\t\t\t\t\t</ ${b}ONLY GROUPS${b} >
-
-${m}[ ${number += 1} ] ${prefix}group${m}
-${m}[ ${number += 1} ] ${prefix}add${m}
-${m}[ ${number += 1} ] ${prefix}kick${m}
-${m}[ ${number += 1} ] ${prefix}promote${m}
-${m}[ ${number += 1} ] ${prefix}demote${m}
-${m}[ ${number += 1} ] ${prefix}adminlist${m}
-${m}[ ${number += 1} ] ${prefix}del${m}
-
-\t\t\t\t\t</ ${b}OWNERS ONLY${b} >
-
-${m}[ ${number += 1} ] >${m}
-${m}[ ${number += 1} ] $${m}
-${m}[ ${number += 1} ] ${prefix}peson${m}
-${m}[ ${number += 1} ] ${prefix}pesoff${m}
-${m}[ ${number += 1} ] ${prefix}mute${m}
-${m}[ ${number += 1} ] ${prefix}unmute${m}
-${m}[ ${number += 1} ] ${prefix}status${m}
-${m}[ ${number += 1} ] ${prefix}setprefix${m}
-${m}[ ${number += 1} ] ${prefix}public${m}
-${m}[ ${number += 1} ] ${prefix}self${m}
-${m}[ ${number += 1} ] ${prefix}setfake${m}
-${m}[ ${number += 1} ] ${prefix}inspect${m}
-${m}[ ${number += 1} ] ${prefix}turnoff${m}
-${m}[ ${number += 1} ] ${prefix}runtime${m}
-${m}[ ${number += 1} ] ${prefix}restart${m}
-${m}[ ${number += 1} ] ${prefix}grouplist${m}
-${m}[ ${number += 1} ] ${prefix}chatall${m}
-${m}[ ${number += 1} ] ${prefix}deletechat${m}
+*Owner*
+${number += 1} > >${m}
+${number += 1} > $${m}
+${number += 1} > ${prefix}peson${m}
+${number += 1} > ${prefix}pesoff${m}
+${number += 1} > ${prefix}mute${m}
+${number += 1} > ${prefix}unmute${m}
+${number += 1} > ${prefix}status${m}
+${number += 1} > ${prefix}setprefix${m}
+${number += 1} > ${prefix}public${m}
+${number += 1} > ${prefix}self${m}
+${number += 1} > ${prefix}setfake${m}
+${number += 1} > ${prefix}inspect${m}
+${number += 1} > ${prefix}turnoff${m}
+${number += 1} > ${prefix}runtime${m}
+${number += 1} > ${prefix}restart${m}
+${number += 1} > ${prefix}grouplist${m}
+${number += 1} > ${prefix}chatall${m}
+${number += 1} > ${prefix}deletechat${m}
 `};
 
 const botstat = (latensi, oldSpeed, newSpeed, kyun, sizeFormat, used, cpus, cpu, os, xcoders, totalChat, groupChat, personalChat, device_manufacturer, device_model, mcc, mnc, os_version, os_build_number, wa_version) => {
